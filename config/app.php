bcd0a159077683c416d29026c5be427c2be9623b
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2018-04-03 10:04:55 +0200

    Changed illuminate/html package to laravelcollective/html

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
 
-        'Illuminate\Html\HtmlServiceProvider',
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,8 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Html'        => 'Illuminate\Html\HtmlFacade',
-        'Form'        => 'Illuminate\Html\FormFacade',
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
