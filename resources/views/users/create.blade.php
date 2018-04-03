d7987d186bbbd983c5733f0747d64d70a349f60a
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2018-04-03 12:27:07 +0200

    Fixed name form in users create view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -37,7 +37,7 @@
     	<div class="col-xs-12 col-sm-12 col-md-12">
     		<div class="form-group">
         		<strong>Email:</strong>
-        		{!! Form::text('name', null, ['placeholder' => 'Name','class' => 'form-control']) !!}
+        		{!! Form::text('email', null, ['placeholder' => 'E-mail','class' => 'form-control']) !!}
     		</div>
     		<div class="col-xs-12 col-sm-12 col-md-12 text-center">
 				<button type="submit" class="btn btn-primary">Submit</button>
