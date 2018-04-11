9dc322d8bc785709f5234880232c472b950f74a4
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2018-04-11 11:26:46 +0200

    Fixed view footer in app

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,28 +2,28 @@
   <div class="container text-center text-md-left">
     <div class="text-center py-4 mb-5">
       <a class="fb-ic" href="#!">
-        <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-facebook fa-lg mr-md-5 mr-3 fa-2x"> </i>
       </a>
       <a class="tw-ic" href="#!">
-        <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-twitter fa-lg mr-md-5 mr-3 fa-2x"> </i>
       </a>
       <a class="gplus-ic" href="#!">
-        <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-google-plus fa-lg mr-md-5 mr-3 fa-2x"> </i>
       </a>
       <a class="li-ic" href="#!">
-        <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-linkedin fa-lg mr-md-5 mr-3 fa-2x"> </i>
       </a>
         <a class="ins-ic" href="#!">
-      <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      <i class="fa fa-instagram fa-lg mr-md-5 mr-3 fa-2x"> </i>
         </a>
       <a class="pin-ic" href="#!">
-        <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+        <i class="fa fa-pinterest fa-lg fa-2x"> </i>
       </a>
     </div>
     <div class="row text-center text-md-left mt-3 pb-3">
       <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
         <h6 class="text-uppercase font-weight-bold">Company name</h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px">
+        <hr class="stress mb-4 mt-0 d-inline-block mx-auto">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec lacus leo. Curabitur aliquam, sem ac ultrices bibendum, ligula sem consequat felis, in auctor dui sapien sit amet velit.</p>
       </div>
       
@@ -31,7 +31,7 @@
       
       <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
         <h6 class="text-uppercase font-weight-bold">Products</h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px">
+        <hr class="stress mb-4 mt-0 d-inline-block mx-auto">
         <p><a href="#!">Landing Pages</a></p>
         <p><a href="#!">Panels</a></p>
         <p><a href="#!">Magento Shops</a></p>
@@ -42,7 +42,7 @@
       
       <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
         <h6 class="text-uppercase font-weight-bold">Useful links</h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px">
+        <hr class="stress mb-4 mt-0 d-inline-block mx-auto">
         <p><a href="#!">Your Account</a></p>
         <p><a href="#!">Become an Affiliate</a></p>
         <p><a href="#!">Shipping Rates</a></p>
@@ -53,7 +53,7 @@
       
       <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
         <h6 class="text-uppercase font-weight-bold">Contact</h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px">
+        <hr class="stress mb-4 mt-0 d-inline-block mx-auto">
         <p><i class="fa fa-home mr-3"></i> Lomza, LOM 18-400, PL</p>
         <p><i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
         <p><i class="fa fa-phone mr-3"></i> + 48 001 002 003</p>
